b20fc9026d197be37e8b34686899b701d85e8fed
abhinay1412 <abhinaykatappagari@gmail.com>
2026-09-03 10:08:02 +0530

    ✅ Daily Temperatures | Medium | Java | Runtime: 71 ms (38.95%) | Memory: 107.6 MB | 2026-08-27

--- leetcode/medium/0739-daily-temperatures/solution.java
+++ leetcode/medium/0739-daily-temperatures/solution.java
@@ -1,34 +1,15 @@
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-
-        int n = temperatures.length;
-        int[] answer = new int[n];
-
-        // Use an array to simulate a stack.
-        // The stack stores indices instead of temperatures.
-        int[] stack = new int[n];
-        int top = -1;
-
-        // Traverse every day's temperature.
-        for (int i = 0; i < n; i++) {
-
-            // If today's temperature is warmer than the day on top of the stack,
-            // we have found the answer for that previous day.
-            while (top >= 0 &&
-                   temperatures[i] > temperatures[stack[top]]) {
-
-                // Get the previous day's index.
-                int previousDay = stack[top--];
-
-                // The waiting time is the distance between the two indices.
-                answer[previousDay] = i - previousDay;
+    public int[] dailyTemperatures(int[] temps) {
+        int[] results = new int[temps.length];
+        Stack<Integer> stack = new Stack<>();
+        /// UPVOTE !
+        for (int i = 0; i < temps.length; i++) {
+            while (!stack.isEmpty() && temps[stack.peek()] < temps[i]) {
+                results[stack.peek()] = i - stack.pop();
             }
-
-            // Today's temperature has not found a warmer future day yet,
-            // so put its index into the stack.
-            stack[++top] = i;
+            stack.push(i);
         }
 
-        return answer;
+        return results;
     }
 }
