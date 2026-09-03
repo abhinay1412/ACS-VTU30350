78247b3a804f17ecc30c3774d44be06efdf323a6
abhinay1412 <abhinaykatappagari@gmail.com>
2026-09-03 10:26:02 +0530

    ✅ Asteroid Collision | Medium | Java | Runtime: 5 ms (67.38%) | Memory: 46.9 MB | 2026-08-29

--- leetcode/medium/0735-asteroid-collision/solution.java
+++ leetcode/medium/0735-asteroid-collision/solution.java
@@ -1,32 +1,32 @@
 class Solution {
-    public int[] asteroidCollision(int[] nums) {
-        int n=nums.length;
-        Stack<Integer> st = new Stack<Integer>();
-        for(int i=0;i<n;i++){
-           if(nums[i]>0){
-            st.push(nums[i]);
-           }
-           else{
-            while(!st.isEmpty()&&st.peek()>0&&st.peek()<Math.abs(nums[i])){
-                st.pop();
-            }
-            if(st.isEmpty()||st.peek()<0){
-                st.push(nums[i]);
-            }
-            if(st.peek()==Math.abs(nums[i])){
-                st.pop();
-            }
-           }
-            
-        }
-
-         int[] ans = new int[st.size()];
-
-        for(int i=st.size()-1;i>=0;i--) {
-            ans[i] = st.pop();
-        }
-
-        return ans;
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
         
+        for (int a : asteroids) {
+            if (a > 0) {
+                stack.push(a);
+            } else {
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -a) {
+                    stack.pop();
+                }
+
+                if (stack.isEmpty() || stack.peek() < 0)  {
+                    stack.push(a);
+                }
+
+                if (stack.peek() == -a) {
+                    stack.pop();
+                }
+            }
+        }
+
+        int[] res = new int[stack.size()];
+        int i = stack.size() - 1;
+
+        while(!stack.isEmpty()) {
+            res[i--] = stack.pop();
+        }
+
+        return res;   
     }
 }
