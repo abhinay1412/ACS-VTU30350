019cc71df0a2f73e3ffe373dc2dbe934268a5f73
abhinay1412 <abhinaykatappagari@gmail.com>
2026-09-03 10:30:02 +0530

    ✅ Online Stock Span | Medium | Java | Runtime: 0 ms | Memory: 42.4 MB | 2026-08-27

--- leetcode/medium/0901-online-stock-span/solution.java
+++ leetcode/medium/0901-online-stock-span/solution.java
@@ -1,14 +1,59 @@
+// Brute force
+
+// class StockSpanner {
+//     List<Integer> list;
+
+//     public StockSpanner() {
+//         list = new ArrayList<>();
+//     }
+    
+//     public int next(int price) {
+//         list.add(price);
+//         int count = 1;
+
+//         for(int i=list.size()-2; i>=0; i--) {
+//             if(list.get(i) <= price) count++;
+//             else break;
+//         }
+
+//         return count;
+//     }
+// }
+
+
+
+
+
+
+
+// Optimal Solution
+
 class StockSpanner {
     Stack<int[]> st;
+    int idx;
+
     public StockSpanner() {
         st = new Stack<>();
+        idx = -1;
     }
+    
     public int next(int price) {
-        int span = 1;
-        while (!st.isEmpty() && st.peek()[0] <= price) {
-            span += st.pop()[1];
+        idx += 1;
+        
+        while(!st.isEmpty() && st.peek()[0] <= price) {
+            st.pop();
         }
-        st.push(new int[] {price, span});
-        return span;
+        
+        int ans = idx - (st.isEmpty() ? -1 : st.peek()[1]);
+        st.push(new int[] {price, idx});
+
+        return ans;
     }
 }
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */ 
