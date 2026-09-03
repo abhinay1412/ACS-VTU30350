75321e9bcf8c57c236f6c92b0ae4f68a309b7cdb
abhinay1412 <abhinaykatappagari@gmail.com>
2026-09-03 10:52:57 +0530

    ✅ Find the Minimum and Maximum Number of Nodes Between Critical Points | Medium | Java | Runtime: 5 ms (40.73%) | Memory: 106 MB | 2026-08-31

--- leetcode/medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.java
+++ leetcode/medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.java
@@ -1,41 +1,41 @@
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        List<Integer> nums = new ArrayList<>();
+        ListNode prev = head;
+        ListNode cur = head.next;
 
-        while(head != null){
-            nums.add(head.val);
-            head = head.next;
+        int idx = 1;
+
+        int firstCritical = -1;
+        int lastCritical = -1;
+        int minDist = Integer.MAX_VALUE;
+
+        while(cur.next != null){
+            ListNode next = cur.next;
+
+            boolean isMax = cur.val > prev.val && cur.val > next.val;
+            boolean isMin = cur.val < prev.val && cur.val < next.val;
+
+            if(isMax || isMin){
+                if(lastCritical == -1){
+                    firstCritical = idx;
+                }
+                else {
+                    minDist = Math.min(minDist, idx - lastCritical);
+                }
+
+                lastCritical = idx;
+            }
+
+            prev = cur;
+            cur = next;
+            idx++;
         }
 
-        List<Integer> criticalPoints = new ArrayList<>();
-
-        int n = nums.size();
-
-        for(int i = 1; i < n - 1; i++){
-            if(nums.get(i) > nums.get(i - 1) && nums.get(i) > nums.get(i + 1)){
-                criticalPoints.add(i);
-            }
-            else if(nums.get(i) < nums.get(i - 1) && nums.get(i) < nums.get(i + 1)){
-                criticalPoints.add(i);
-            }
-        }
-
-        int m = criticalPoints.size();
-
-        if(m < 2){
+        if(firstCritical == -1 || firstCritical == lastCritical){
             return new int[]{-1, -1};
         }
 
-        int minDist = Integer.MAX_VALUE;
-
-        int maxDist = criticalPoints.get(m - 1) - criticalPoints.get(0);
-
-        for(int i = 1; i < m; i++){
-            minDist = Math.min(
-                minDist,
-                criticalPoints.get(i) - criticalPoints.get(i - 1)
-            );
-        }
+        int maxDist = lastCritical - firstCritical;
 
         return new int[]{minDist, maxDist};
     }
