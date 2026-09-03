b6a9678fc15238effd8942d27f39959ed4c99bb5
abhinay1412 <abhinaykatappagari@gmail.com>
2026-09-03 10:25:05 +0530

    ✅ Sliding Window Maximum | Hard | Java | Runtime: 0 ms | Memory: 43 MB | 2026-08-29

--- leetcode/hard/0239-sliding-window-maximum/solution.java
+++ leetcode/hard/0239-sliding-window-maximum/solution.java
@@ -1,33 +1,26 @@
+
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> dq=new ArrayDeque<>();
-        int i=0;
-        int j=0;
-        while(i-j!=k){
-            while(!dq.isEmpty() && dq.peekFirst()<nums[i]){
-                dq.removeFirst();
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> window = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!window.isEmpty() && window.peekFirst() < i - k + 1) {
+                window.pollFirst();
             }
-            if(dq.isEmpty() || dq.peekFirst()>=nums[i]){
-                dq.addFirst(nums[i]);
+
+            while (!window.isEmpty() && nums[window.peekLast()] < nums[i]) {
+                window.pollLast();
             }
-            i++;
+
+            window.offerLast(i);
+
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[window.peekFirst()];
+            }
         }
-        int [] ans=new int[nums.length-k+1];
-        ans[j]=dq.peekLast();
-        while(i!=nums.length){
-            if(dq.peekLast()==nums[j] ){
-                dq.removeLast();
-            }
-            j++;
-            while(!dq.isEmpty() && dq.peekFirst()<nums[i]){
-                dq.removeFirst();
-            }
-            if(dq.isEmpty() || dq.peekFirst()>=nums[i]){
-                dq.addFirst(nums[i]);
-            }
-            i++;
-            ans[j]=dq.peekLast();
-        }
-        return ans;
+
+        return result;
     }
 }
