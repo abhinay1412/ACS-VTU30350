7e4e312ce8796a03fc5009ee4876b6d0fc05c07b
abhinay1412 <abhinaykatappagari@gmail.com>
2026-09-03 09:55:02 +0530

    ✅ Longest Palindromic Substring | Medium | Java | Runtime: 15 ms (59.13%) | Memory: 43.7 MB | 2026-08-24

--- leetcode/medium/0005-longest-palindromic-substring/solution.java
+++ leetcode/medium/0005-longest-palindromic-substring/solution.java
@@ -1,31 +1,24 @@
-public class Solution {
-    public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
-        }
-
-        String maxStr = s.substring(0, 1);
-
-        for (int i = 0; i < s.length() - 1; i++) {
-            String odd = expandFromCenter(s, i, i);
-            String even = expandFromCenter(s, i, i + 1);
-
-            if (odd.length() > maxStr.length()) {
-                maxStr = odd;
-            }
-            if (even.length() > maxStr.length()) {
-                maxStr = even;
-            }
-        }
-
-        return maxStr;
-    }
-
-    private String expandFromCenter(String s, int left, int right) {
+class Solution {
+    public int expand(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-        return s.substring(left + 1, right);
+        return right - left - 1;
+    }
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
     }
 }
