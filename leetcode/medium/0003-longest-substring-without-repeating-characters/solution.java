3e90eec20aa6822a82796adcfaf78480969991a7
abhinay1412 <abhinaykatappagari@gmail.com>
2026-09-07 11:36:34 +0530

    ✅ Longest Substring Without Repeating Characters | Medium | Java | Runtime: 50 ms (44.96%) | Memory: 47.8 MB | 2026-09-07

--- leetcode/medium/0003-longest-substring-without-repeating-characters/solution.java
+++ leetcode/medium/0003-longest-substring-without-repeating-characters/solution.java
@@ -1,18 +1,20 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        int l=0, r=0, maxLen=0;
         int n = s.length();
-        while(r<n)
-        {
-            if(map.containsKey(s.charAt(r)))
-            l = Math.max(l,map.get(s.charAt(r))+1);
-
-            int len = r-l+1;
-            maxLen = Math.max(len,maxLen);
-            map.put(s.charAt(r),r);
-            r++;
+        int maxLength = 0;
+        Map<Character, Integer> charMap = new HashMap<>();
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            if (!charMap.containsKey(s.charAt(right)) || charMap.get(s.charAt(right)) < left) {
+                charMap.put(s.charAt(right), right);
+                maxLength = Math.max(maxLength, right - left + 1);
+            } else {
+                left = charMap.get(s.charAt(right)) + 1;
+                charMap.put(s.charAt(right), right);
+            }
         }
-        return maxLen;
+        
+        return maxLength;
     }
 }
