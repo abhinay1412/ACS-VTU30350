648e07f9a6edc40a5e58347f8b1af4bbc565d418
abhinay1412 <abhinaykatappagari@gmail.com>
2026-09-21 11:03:40 +0530

    ✅ Design Parking System | Easy | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-09-21

--- leetcode/easy/1603-design-parking-system/solution.java
+++ leetcode/easy/1603-design-parking-system/solution.java
@@ -1,34 +1,27 @@
 class ParkingSystem {
-
-    int big; int medium;
-    int small;
-
+    private int big,small,medium;
     public ParkingSystem(int big, int medium, int small) {
-        this.big = big;
-        this.medium = medium;
-        this.small = small;
+        
+       this.big = big;
+       this.medium = medium;
+       this.small= small; 
     }
     
     public boolean addCar(int carType) {
-        if(carType == 1){
-            if(big > 0){
-                big--; return true;
-            }
+        if(carType == 1 && big>0){
+            big-=1;
+            return true;
         }
-
-        else if(carType == 2){
-            if(medium > 0){
-                medium--;
-                return true;
-            }
+        else if(carType==2 && medium>0){
+            medium-=1;
+            return true;
         }
-
-        else{
-            if(small > 0){
-                small--;
-                return true;
-            }
+        else if(carType==3 && small>0){
+            small-=1;
+            return true; 
         }
         return false;
+        
+        
     }
 }
