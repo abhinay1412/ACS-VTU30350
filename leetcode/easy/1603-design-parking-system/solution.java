cee95deb4a04b89bc6417a4e40e4cc2128d20f21
abhinay1412 <abhinaykatappagari@gmail.com>
2026-09-21 11:38:36 +0530

    ✅ Design Parking System | Easy | Java | Runtime: 7 ms (100.00%) | Memory: 46.8 MB | 2026-09-21

--- leetcode/easy/1603-design-parking-system/solution.java
+++ leetcode/easy/1603-design-parking-system/solution.java
@@ -1,27 +1,17 @@
-class ParkingSystem {
-    private int big,small,medium;
+    class ParkingSystem {
+
+    int[] arr ;
     public ParkingSystem(int big, int medium, int small) {
-        
-       this.big = big;
-       this.medium = medium;
-       this.small= small; 
+        arr = new int[]{big, medium, small};
     }
-    
+
     public boolean addCar(int carType) {
-        if(carType == 1 && big>0){
-            big-=1;
-            return true;
-        }
-        else if(carType==2 && medium>0){
-            medium-=1;
-            return true;
-        }
-        else if(carType==3 && small>0){
-            small-=1;
-            return true; 
-        }
-        return false;
-        
-        
+        return (arr[carType-1]-- > 0)? true : false;
     }
 }
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
